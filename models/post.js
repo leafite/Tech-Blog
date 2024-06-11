beb531de225d9836ba6bffcf191c0e8109ab97d7
Christopher Bautista <cbaut@Christophers-Laptop.local>
2024-06-10 23:50:39 -0700

    added code

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+});
+
+const Post = sequelize.define('Post', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  }
+});
+
+module.exports = Post;
