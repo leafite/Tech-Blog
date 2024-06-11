bc2bfcb18e5b62fbcec1057c7bb2a26d3f9efa7f
Christopher Bautista <cbaut@Christophers-Laptop.local>
2024-06-10 23:50:12 -0700

    comment js

---
+++ models/comment.js
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+});
+
+const Comment = sequelize.define('Comment', {
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  postId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  }
+});
+
+module.exports = Comment;
